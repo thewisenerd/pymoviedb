f39285db6692be011ff8179597c616d66983ed97
thewisenerd <thewisenerd@protonmail.com>
2015-12-26 21:00:19 +0530

    add omdb-0.5.0
    Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

---
+++ src/omdb-0.5.0/CHANGES.rst
@@ -0,0 +1,74 @@
+Changelog
+=========
+
+
+v0.5.0 (2015-07-29)
+-------------------
+
+- Add support for ``Season``/``Episode`` OMDb parameter via ``season``/``episode`` arguments to every main API function. Thanks cihansahin_!
+
+
+v0.4.0 (2015-04-29)
+-------------------
+
+- Add ``Season``, ``Episode``, and ``SeriesID`` OMDb API fields as ``season``, ``episode``, and ``series_id`` model fields.
+
+
+v0.3.1 (2015-01-27)
+-------------------
+
+- Add metadata to main module:
+
+    - ``__title__``
+    - ``__summary__``
+    - ``__url__``
+    - ``__version__``
+    - ``__author__``
+    - ``__email__``
+    - ``__license__``
+
+
+v0.3.0 (2015-01-13)
+-------------------
+
+- Add ``search_movie``.
+- Add ``search_episode``.
+- Add ``search_series``.
+- Add support for ``type`` OMDb parameter via ``media_type`` argument to every main API function.
+
+
+v0.2.0 (2014-10-16)
+-------------------
+
+- Update ``models.Item`` with additional ``OMDb API`` fields: ``Awards``, ``Country``, ``Language``, and ``Metascore``.
+- Add ``omdb.request`` method for easier access to raw request response.
+- Initialization of ``omdb.Client`` now accepts keyword arguments for API request parameter defaults. Previously, a ``dict`` object needed to be passed in.
+- Full PEP8 compliance.
+- Integrate ``tox`` testing into ``setup.py``.
+
+
+v0.1.1 (2014-02-09)
+-------------------
+
+- Python3 support. Thanks agronholm_!
+- PEP8 compliance excluding max-line-length. Thanks agronholm_!
+- Wheel support. Thanks agronholm_!
+
+
+v0.1.0 (2013-11-24)
+-------------------
+
+- Convert API response to data models (see omdb/models.py).
+- Add /tests folder and move appropriate doctests there.
+- Return empty data for ``search`` and ``get`` requests which return no record(s).
+- Add ``omdb.set_default()`` for setting default request parameters (e.g. ``set_default(tomatoes=True)`` to always include tomatoes data)
+
+
+v0.0.1 (2013-11-12)
+-------------------
+
+- Initial release.
+
+
+.. _agronholm: https://github.com/agronholm
+.. _cihansahin: https://github.com/cihansahin
---
+++ src/omdb-0.5.0/omdb/client.py
@@ -0,0 +1,104 @@
+"""OMDb API client.
+"""
+
+import requests
+
+from . import models
+from ._compat import iteritems
+
+
+class Client(object):
+    """HTTP request client for OMDB API."""
+
+    url = 'http://www.omdbapi.com'
+
+    params_map = {
+        's': 'search',
+        't': 'title',
+        'i': 'imdbid',
+        'y': 'year',
+        'Season': 'season',
+        'Episode': 'episode',
+        'plot': 'plot',
+        'type': 'type',
+        'tomatoes': 'tomatoes'
+    }
+
+    def __init__(self, **defaults):
+        self.default_params = defaults
+        self.session = requests.Session()
+
+    def set_default(self, key, default):
+        """Set default request params."""
+        self.default_params[key] = default
+
+    def convert_params(self, params):
+        """Map OMDb params to our renaming."""
+        _params = {}
+
+        for api_arg, arg in iteritems(self.params_map):
+            if arg in params:
+                _params[api_arg] = params[arg]
+
+        return _params
+
+    def request(self, **params):
+        """HTTP GET request to OMDB API.
+
+        Raises exception for non-200 HTTP status codes.
+        """
+        res = self.session.get(self.url, params=params)
+
+        # raise HTTP status code exception if status code != 200
+        # if status_code == 200, then no exception raised
+        res.raise_for_status()
+
+        return res
+
+    def get(self,
+            search=None,
+            title=None,
+            imdbid=None,
+            year=None,
+            fullplot=None,
+            tomatoes=None,
+            media_type=None,
+            season=None,
+            episode=None):
+        """Generic request returned as dict."""
+
+        params = {
+            'search': search,
+            'title': title,
+            'imdbid': imdbid,
+            'year': year,
+            'type': media_type,
+            'plot': 'full' if fullplot else 'short',
+            'tomatoes': 'true' if tomatoes else False,
+            'season': season,
+            'episode': episode
+        }
+
+        # remove falsey params
+        params = dict([(k, v) for k, v in iteritems(params) if v])
+
+        # set defaults
+        for key, value in iteritems(self.default_params):
+            params.setdefault(key, value)
+
+        # convert function args to API query params
+        params = self.convert_params(params)
+
+        data = self.request(**params).json()
+
+        return self.set_model(data, params)
+
+    def set_model(self, data, params):  # pylint: disable=no-self-use
+        """Convert data into first class models."""
+        if 's' in params:
+            # omdbapi returns search results even if imdbid supplied
+            data = models.Search(data)
+        else:
+            data = models.Item(data)
+
+        return data
